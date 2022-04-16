c5f404e4c73fd57fe114f2e92b7f1f55dc02db96
Gençay Yıldız <gyildizmail@gmail.com>
2022-04-16 18:44:47 +0300

    Table Per Hierarchy - Example Code

---
+++ EFCoreTPHExample/Migrations/20220416153000_mig_1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreTPHExample.Migrations
+{
+    public partial class mig_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    School = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Branch = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ EFCoreTPHExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using TPHExampleDb context = new();
+//await context.Students.AddAsync(new() { Name = "Hilmi", School = "Hacı Hüseyin İmam Hatip Lisesi" });
+//await context.Students.AddAsync(new() { Name = "Rauf", School = "Laylaylom Galiba Sana Göre Sevmeler Lisesi" });
+//await context.Teachers.AddAsync(new() { Name = "Emine", Branch = "Fizik" });
+//await context.Teachers.AddAsync(new() { Name = "Muiddin", Branch = "Edebiyat" });
+//await context.SaveChangesAsync();
+
+
+var students =  context.Users.OfType<Student>();
+var teachers =  context.Users.OfType<Teacher>();
+
+//Entity Framework Core - Table Per Hierarchy(TPH) 
+public class ApplicationUser
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+public class Student : ApplicationUser
+{
+    public string School { get; set; }
+}
+public class Teacher : ApplicationUser
+{
+    public string Branch { get; set; }
+}
+public class TPHExampleDb : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer($"Server=localhost, 1433;User ID=sa;Password=23asjDnsd23'^;Database={nameof(TPHExampleDb)}");
+    }
+    public DbSet<ApplicationUser> Users { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Teacher> Teachers { get; set; }
+}
